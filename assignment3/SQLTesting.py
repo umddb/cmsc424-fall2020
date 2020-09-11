1ec879599190847393500c52e6d5b2d5099f6a1e
Amol Deshpande <amolvdeshpande@gmail.com>
2020-09-11 17:21:11 -0400

    assignment 3

--- assignment3/SQLTesting.py
+++ assignment3/SQLTesting.py
@@ -8,6 +8,9 @@ import argparse
 
 from queries import *
 
+
+correct_answers = []
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--interactive', help="Run queries one at a time, and wait for user to proceed", required=False, action="store_true")
 parser.add_argument('-q', '--query', type = int, help="Only run the given query number", required=False)
@@ -19,7 +22,7 @@ conn = psycopg2.connect("dbname=elections user=vagrant")
 cur = conn.cursor()
 
 totalscore = 0
-for i in range(0, 17):
+for i in range(0, 15):
     # If a query is specified by -q option, only do that one
     if args.query is None or args.query == i:
         try:
@@ -29,18 +32,19 @@ for i in range(0, 17):
             print(queries[i])
             cur.execute(queries[i])
 
-            if i not in [14, 15, 16]:
+            if i not in [13, 14]:
                 ans = cur.fetchall()
+                correct_answers.append(ans)
 
                 print("--------- Your Query Answer ---------")
                 for t in ans:
                     print(t)
                 print("")
             else:
-                if i in [14, 15]:
+                if i in [13]:
                     conn.commit()
-                    print("--------- Running SELECT * FROM Counties LIMIT 5 -------")
-                    cur.execute("select * from counties limit 5")
+                    print("--------- Running SELECT * FROM pres_state_votes LIMIT 5 -------")
+                    cur.execute("select * from pres_state_votes limit 5")
                     ans = cur.fetchall()
                     print("-- Result")
                     for t in ans:
@@ -48,10 +52,11 @@ for i in range(0, 17):
                     print("")
                 else:
                     conn.commit()
-                    print("--------- Running SELECT count(*) FROM pres_county_returns where partyname not in ('democrat', 'republican') -------")
-                    cur.execute("select count(*) from pres_county_returns where partyname not in ('democrat', 'republican')")
+                    print("--------- Listing constraints for 'counties' -------")
+                    cur.execute(" SELECT conname, contype FROM pg_catalog.pg_constraint con INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace WHERE rel.relname = 'counties' ")
+
                     ans = cur.fetchall()
-                    print("-- Result (should be 0)")
+                    print("-- Result should list the 4 constraints with appropriate names")
                     for t in ans:
                         print(t)
                     print("")
@@ -62,3 +67,7 @@ for i in range(0, 17):
         except:
             print(sys.exc_info())
             raise
+            
+
+
+
--- assignment3/queries.py
+++ assignment3/queries.py
@@ -1,4 +1,4 @@
-queries = ["" for i in range(0, 17)]
+queries = ["" for i in range(0, 15)]
 
 ### 0. Report the votes for the normal (i.e, not special) Senate Election in Maryland in 2018.
 ### Output column order: candidatename, partyname, candidatevotes
@@ -10,175 +10,138 @@ where year = 2018 and statecode = 'MD' and specialelections = False
 order by candidatename asc;
 """
 
-### 1. Report the number of votes for candidate 'Ben Cardin' across all the senate elections.
-### Output Column: year, statecode, specialelections, candidatevotes 
-### Order by candidatevotes increasing
+### 1. Write a query to find the maximum, minimum, and average population in 2010 across all states.
+### The result will be a single row.
+### Truncate the avg population to a whole number using trunc
+### Output Columns: max_population, min_population, avg_population
 queries[1] = """
 select 0;
 """
 
-
-### 2. Write a query to output the % increase, truncated to whole integer using TRUNC, in the population from 1950 to 2010.
-### So for Autauga, the answer would be: 200  (54571-18186)*100/18186 = 200.7 ==> truncated to 200
-### There are some counties with 0 population in 1950 -- remove those counties from the answer.
-### Output columns: countyname, statecode, percentincrease
-### Order output by precentincrease increasing
+### 2. Write a query to find the candidate with the maximum votes in the 2008 MI Senate Election. 
+### Output Column: candidatename
+### Order by: candidatename
 queries[2] = """
 select 0;
 """
 
-### 3. Select all the "distinct" party names that senate candidates have been affiliated over all
-### the elections. 
-### Output column: partyname
-### Order output by partyname ascending
+### 3. Write a query to find the number of candidates who are listed in the sen_state_returns table for each senate election held in 2018. 
+### Note that there may be two elections in some states, and there should be two tuples in the output for that state.
+### 'NA' or '' (empty) should be treated as candidates. 
+### Output columns: statecode, specialelections, numcandidates
+### Order by: statecode, specialelections
 queries[3] = """
 select 0;
 """
 
-### 4. Write a query to output for each state how many years ago it was admitted to the union. 
-### So if a state was admitted to the union in 1819, the answer would 201 (2020 - 1819). Ignore the specific dates.
-### Output columns: name, admittedduration
-### Order by admittedduration decreasing
+### 4. Write a query to find, for the 2008 elections, the number of counties where Barack Obama received strictly more votes 
+### than John McCain.
+### This will require you to do a self-join, i.e., join pres_county_returns with itself.
+### Output columns: num_counties
 queries[4] = """
 select 0;
 """
 
-### 5. Write a query to find the states where the increase in population from 1900 to 1950 was lower than the increase in population from 2000 to 2010.
-### Output Column: name
-### Order by: name increasing
+
+### 5. Write a query to find the names of the states with at least 100 counties in the 'counties' table.
+### Use HAVING clause for this purpose.
+### Output columns: statename, num_counties
+### Order by: statename
 queries[5] = """
 select 0;
 """
 
-### 6. Write a query to find all candidates for senate who satisfy one of the following conditions:
-###        - the candidate is a 'democrat' and has more than 750000 votes in Alabama.
-###        - the candidate is a 'republican' and has more 1,000,000 votes in Maryland.
-###        - the candidate is neither a democrat or nor a republican and has more than 500,000 votes (in any state).
-### Some candidates appear under multiple party names. Ignore that for now (in other words, if a democrat has 700,000 votes in AL as a 'democrat' and
-### also gets 100,000 votes as something else, that candidate should NOT be in the result
-### Also: ignore any party names like 'democratic-farmer-labor' etc.
-### Output columns: year, statecode, specialelections, candidatename, partyname
+### 6. Write a query to construct a table:
+###     (statecode, total_votes_2008, total_votes_2012)
+### to count the total number of votes by state for Barack Obama in the two elections.
+###
+### Use the ability to "sum" an expression (e.g., the following query returns the number of counties in 'AR')
+### select sum(case when statecode = 'AR' then 1 else 0 end) from counties;
+###
+### Order by: statecode
 queries[6] = """
 select 0;
 """
 
-
-### 7. Write a query to join the tables states and counties to create a list of county names, county population in 2010, state name, the state
-### population in 2010
-### Output columns: statename, statepopulation, countyname, countypopulation
-### Order first by statename, then by countyname, increasing
+### 7. Create a table to list the disparity between the populations listed in 'states' table and those listed in 'counties' table for 1950 and 2010.
+### Result should be: 
+###        (statename, disparity_1950, disparity_2010)
+### So disparity_1950 = state population 1950 - sum of population_1950 for the counties in that state
+### Use HAVING to only output those states where there is some disparity (i.e., where at least one of the two is non-zero)
+### Order by statename
 queries[7] = """
 select 0;
 """
 
-### 8. Write a query to join the tables states and counties to find the counties that had over
-### 50% of the population of the state in 2010
-### Output columns: statename, countyname 
-### Order by statename, then by countyname, increasing
+### 8. Use 'EXISTS' or 'NOT EXISTS' to find the states where no counties have population in 2010 above 500000 (500 thousand).
+### Output columns: statename
+### Order by statename
 queries[8] = """
 select 0;
 """
 
-
-### 9. Write a query to join sen_state_returns and sen_elections to find the candidates that received 70% or more of the total vote.
-### Output columns: year, statecode, specialelections, candidatename
-### Order by percentage of total vote increasing
+### 9. List all counties and their basic information that have a unique name across all states. 
+### Use scalar subqueries to simplify the query.
+### Output columns: all attributes of counties (name, statecode, population_1950, population_2010)
+### Order by name, statecode
 queries[9] = """
 select 0;
 """
 
-
-### 10. The tables were collected from 2 different sources, and there may be some consistency issues across them. 
-### Write a query to find all counties (and the corresponding state names) that are present in "pres_county_returns" table, but 
-### do not have any corresponding entry in the "counties"
-### table (through straightforward string equality -- so 'Autauga' and 'Autauga ' (with an extra space) would NOT be considered a match.
-### Each county+state combination should only appear once in the output.
-### HINT: Use "not in".
-### Output Columns: countyname, statename
-### Order by countyname, statename ascending
+### 10. Use Set Intersection to find the counties that Barack Obama lost in 2008, but won in 2012.
+### We have created a temporary table using WITH that you can use (or not).
+###
+### Output columns: countyname, statecode
+### Order by countyname, statecode
 queries[10] = """
-select 0;
+with temp1 as (select countyname, statecode, year, max(candidatevotes) as maxvotes
+        from pres_county_returns 
+        group by countyname, statecode, year
+        ),
+     temp2 as (
+             select p.countyname, p.statecode, p.year, p.candidatevotes = t.maxvotes as won
+             from pres_county_returns p, temp1 t
+             where p.countyname = t.countyname and p.statecode = t.statecode and p.year = t.year and p.candidatename = 'Barack Obama'
+)
+select * 
+from temp2;
 """
 
 
-### 11. For the 2012 presidential elections and for 'Barack Obama', write a
-### query to combine pres_county_returns and counties so that, to produce a result
-### with the following columns:
-###     countyname, statecode, candidatevotes, population_2010
-### However, for the counties in pres_county_returns that do not have a match in 
-### 'counties' table, we want population_2010 to be set to NULL.
-### Use a left (or right) outer join to achive this.
-### Output: countyname, statecode, candidatevotes, population_2010
-### Order by: countyname, statecode ascending
+### 11. Find all presidential candidates listed in pres_county_returns who did NOT run for elections as a senator.
+### HINT: Use "except" to simplify the query
+###
+### Every candidate should be reported only once. You will see incorrect answers in there because "names" don't match -- that's fine.
+###
+### Output columns: candidatename
+### Order by: candidatename
 queries[11] = """
 select 0;
 """
 
 
-### 12. SQL "with" clause can be used to simplify queries. It essentially allows
-### specifying temporary tables to be used during the rest of the query. See Section
-### 3.8.6 (6th Edition) for some examples.
+
+### 12. Create a table listing the months and the number of states that were admitted to the union (admitted_to_union field) in that month.
+### Use 'extract' for operating on dates, and the ability to create a small inline table in SQL. For example, try:
+###         select * from (values(1, 'Jan'), (2, 'Feb')) as x;
 ###
-### Below we are providing a part of a query that uses "with" to create a
-### temporary table where, for 2000 elections, we are finding the maximum of the
-### candidate votes for each county. Join this temporary table with the
-### 'pres_county_returns' table to find the winner for each county. 
-### This is unfortunately the easiest way to do this task.
-###
-### You don't need to fully understand what the 'temp' query does to do the join
-### -- as provided, the query shows you the result of the "temp" table
-### Output columns: countyname, statecode, candidatename
-### Order by: countyname, statecode
+### Output columns: month_no, monthname, num_states_admitted
+### month should take values: Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
+### Order by: month_no
 queries[12] = """
-with temp as (select countyname, statecode, max(candidatevotes) as maxvotes
-        from pres_county_returns
-        where year = 2000
-        group by countyname, statecode)
-select *
-from temp t;
+select 0;
 """
 
 
-### 13. Let's create a table showing the vote differences between 2000 and 2016 for
-### the democratic presidential candidates. So the result should be four columns:
-###        statecode, countname2000, countyname2016,  votes2000, votes2016
-### However, the list of counties is different, we want the other one to be set
-### to None. We will do this through use of two temporary tables using WITH
-### clause. You have to complete this query using a "full outer join" to get the 
-### desired result.
-###
-### Output: statecode2000 (from temp1), statecode2016 (from temp2), countname2000, countyname2016, votes2000, votes2016
-### Order by: statecode2000, statecode2016, countyname2000, countyname2016 ascending
+### 13. Create a view pres_state_votes with schema (year, statecode, candidatename, partyname, candidatevotes) where we maintain aggregated counts by statecode (i.e.,
+### candidatevotes in this view would be the total votes for each state, including states with statecode 'NA').
 queries[13] = """
-with temp1 as (select countyname, statecode, candidatevotes
-               from pres_county_returns
-               where partyname = 'democrat' and year = 2000),
-temp2 as (select countyname, statecode, candidatevotes
-          from pres_county_returns
-          where partyname = 'democrat' and year = 2016)
-select *
-from temp1;
+select 0;
 """
 
-
-
-### 14. Write a statement to add a new column to the counties table called 'pop_trend' of type 'string'.
+### 14. Use a single ALTER TABLE statement to add (name, statecode) as primary key to counties, and to add CHECKs that neither of the two populations are less than zero.
+### Name the two CHECK constraints nonzero2010 and nonzero1950.
 queries[14] = """
 select 0;
 """
 
-### 15. The values for the above added column with be empty to begin with. Write an update statement to 
-### set it to 'decreased', 'increased somewhat', and 'increased a lot', depending on whether the population decreased,
-### increased by less than a factor of 2 (i.e., population_2010 <= 2*population_2950), or increased by a factor of more than 2.
-### Use CASE statement to make this easier.
-queries[15] = """
-select 0;
-"""
-
-
-### 16. Write a statement to delete all tuples from pres_county_returns where the party is neither 'democrat' not 'republican'.
-### NOTE: Keep in mind that you will need repopulate the table if you are trying to test the earlier
-### queries after this delete.
-queries[16] = """
-select 0;
-"""
